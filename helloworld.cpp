615c3a3a7398e90e0f13b2b68a66412fe2a37385
Jordan Coppert <coppert@usc.edu>
2016-08-23 15:25:16 -0700

    Added two more lines

--- helloworld.cpp
+++ helloworld.cpp
@@ -3,4 +3,6 @@
 int main() {
         std::cout << "HelloWorld!" << std::endl;
         std::cout << "Get ready for some fun in 104!" << std::endl;
+        std::cout << "Round 2" << std::endl;
+        std::cout << "Fight On!" << std::endl;
 }
